a12b4b8361918d78ac7c1158a90a037ac032eb46
Marlus Dias Silva <marlus@new-host-5.home>
2017-11-18 11:37:25 -0200

    atual

--- asasCerrado/src/pages/paramotor/paramotor.module.ts
+++ asasCerrado/src/pages/paramotor/paramotor.module.ts
@@ -1,6 +1,16 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { ParamotorPage } from './paramotor';
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent,
+  GoogleMapOptions,
+  CameraPosition,
+  MarkerOptions,
+  Marker
+} from '@ionic-native/google-maps';
+import { Component } from "@angular/core/";
 
 @NgModule({
   declarations: [
@@ -10,4 +20,55 @@ import { ParamotorPage } from './paramotor';
     IonicPageModule.forChild(ParamotorPage),
   ],
 })
-export class ParamotorPageModule {}
+export class ParamotorPageModule {
+  map: GoogleMap;
+  constructor(private googleMaps: GoogleMaps) { }
+
+  ionViewDidLoad() {
+    this.loadMap();
+  }
+
+  ngAfterViewInit() {
+    this.loadMap();
+  }
+
+  loadMap() {
+
+    let mapOptions: GoogleMapOptions = {
+      camera: {
+        target: {
+          lat: 43.0741904,
+          lng: -89.3809802
+        },
+        zoom: 18,
+        tilt: 30
+      }
+    };
+
+    this.map = this.googleMaps.create('map_canvas', mapOptions);
+
+    // Wait the MAP_READY before using any methods.
+    this.map.one(GoogleMapsEvent.MAP_READY)
+      .then(() => {
+        console.log('Map is ready!');
+
+        // Now you can use all methods safely.
+        this.map.addMarker({
+          title: 'Ionic',
+          icon: 'blue',
+          animation: 'DROP',
+          position: {
+            lat: 43.0741904,
+            lng: -89.3809802
+          }
+        })
+          .then(marker => {
+            marker.on(GoogleMapsEvent.MARKER_CLICK)
+              .subscribe(() => {
+                alert('clicked');
+              });
+          });
+
+      });
+  }
+}
